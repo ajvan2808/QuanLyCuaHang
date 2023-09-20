a65b4cbc5f90060905c653ee82b3f4a56ef11bd7
ajvan2808 <vhh2808@gmail.com>
2023-09-21 01:44:51 +0700

    insert data

--- data/add_data.py
+++ data/add_data.py
@@ -1,59 +1,75 @@
 import csv
+import uuid
 import pymysql
 
 mydb = pymysql.connect(
     host='localhost',
     user='root',
     passwd='',
-    database='quanlycuahang',
+    database='quanlykhohang',
     port=3306
 )
 
 cursor = mydb.cursor()
-# Nhap data tu customers.csv vao bang KhachHang
-with open("customers.csv", mode='r') as custfile:
-    reader = csv.reader(custfile)
+
+'''
+Lưu ý:
+- Thứ tự tạo bảng và thêm data vào bảng do có ràng buộc foreign keys
+'''
+
+with open("KhachHang.csv", mode='r') as file1:
+    reader = csv.reader(file1)
     next(reader)  # skip header
     for row in reader:
-        sql = "INSERT INTO KhachHang (ma_khach_hang, ten_khach_hang, dia_chi, dien_thoai, email) VALUES (%s, %s, %s, %s, %s)"
-        val = (row[0], row[1], row[3], row[2], row[4])
+        sql = ("INSERT INTO KhachHang (ma_khach_hang, ten_khach_hang, dien_thoai, dia_chi, ngay_sinh, doanh_thu) "
+               "VALUES (%s, %s, %s, %s, %s, %s)")
+        val = (row[0], row[1], row[2], row[3], row[4], row[5])
         cursor.execute(sql, val)
         mydb.commit()
 
-with open("orders.csv", mode='r') as odfile:
-    reader = csv.reader(odfile)
+with open("NhanVien.csv", mode='r') as file1:
+    reader = csv.reader(file1)
     next(reader)  # skip header
     for row in reader:
-        sql = "INSERT INTO DonHang (ma_don_hang, so_luong, tong_tien, ngay_dat_hang) VALUES (%s, %s, %s, %s)"
-        val = (row[0], row[2], row[3], row[1])
+        sql = ("INSERT INTO NhanVien (ma_nhan_vien, ho_ten, dien_thoai, dia_chi, ngay_vao_lam)"
+               "VALUES (%s, %s, %s, %s, %s)")
+        val = (row[0], row[1], row[2], row[3], row[4])
         cursor.execute(sql, val)
         mydb.commit()
 
-with open("order_details.csv", mode='r') as odtfile:
-    reader = csv.reader(odtfile)
+with open("DonHang.csv", mode='r') as dhfile:
+    reader = csv.reader(dhfile)
     next(reader)  # skip header
     for row in reader:
-        sql = "INSERT INTO ChiTietDonHang (ma_thong_tin_dh, don_vi, gia) VALUES (%s, %s, %s)"
-        val = (row[0], row[3], row[4])
+        sql = "INSERT INTO DonHang (ma_don_hang, ngay_tao_dh, tong_so_luong, tong_tien, ma_kh, ma_nv) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (row[0], row[1], row[5], row[4], row[2], row[3])
         cursor.execute(sql, val)
         mydb.commit()
 
-with open("products.csv", mode='r') as prodfile:
-    reader = csv.reader(prodfile)
+with open("SanPham.csv", mode='r') as spfile:
+    reader = csv.reader(spfile)
     next(reader)  # skip header
     for row in reader:
-        sql = "INSERT INTO SanPham (ma_san_pham, ten_san_pham, phan_loai, gia, mo_ta, ngay_nhap_hang, ngay_xuat_hang) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (row[0], row[1], row[2], row[3], row[6], row[4], row[5])
+        sql = "INSERT INTO SanPham (ma_san_pham, ten_san_pham, don_vi_tinh, nuoc_san_xuat, han_su_dung, gia) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (row[0], row[1], row[2], row[3], row[4], row[5])
         cursor.execute(sql, val)
         mydb.commit()
 
 
-# for _ in range(2500):
-#     with open("orders.csv", mode='r') as odtfile:
-#         reader = csv.reader(odtfile)
-#         next(reader)  # skip header
-#         for row in reader:
-#             sql = "SET FOREIGN_KEY_CHECKS=0; INSERT INTO ChiTietDonHang (ma_dh) VALUES ('', SELECT)"
-#             val = (row[0])
-#             cursor.execute(sql, val)
-#             mydb.commit()
+with open("ChiTietDonHang.csv", mode='r') as ctdhfile:
+    reader = csv.reader(ctdhfile)
+    next(reader)  # skip header
+    for row in reader:
+        sql = "INSERT INTO ChiTietDonHang (ma_chi_tiet_dh, so_luong, ma_dh, ma_sp) VALUES (%s,%s, %s, %s)"
+        val = (str(str(uuid.uuid4())[:8]), row[2], row[0], row[1])
+        cursor.execute(sql, val)
+        mydb.commit()
+
+with open("KhoHang.csv", mode='r') as khfile:
+    reader = csv.reader(khfile)
+    next(reader)  # skip header
+    for row in reader:
+        sql = "INSERT INTO KhoHang (ten_san_pham, don_vi_tinh, nuoc_san_xuat, han_su_dung, gia, so_luong, trang_thai, ma_sp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[0])
+        cursor.execute(sql, val)
+        mydb.commit()
--- models.py
+++ models.py
@@ -1,60 +1,137 @@
+import uuid
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, Float, Integer, ForeignKey, create_engine, DECIMAL, DateTime
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, BigInteger
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
+
+
+from sqlalchemy.types import TypeDecorator, CHAR
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+    Uses PostgreSQL's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+    """
+
+    impl = CHAR
+    cache_ok = True
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == "postgresql":
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
 
 
 class KhachHang(Base):
     __tablename__ = 'KhachHang'
     ma_khach_hang = Column(String(200), nullable=False, primary_key=True)
     ten_khach_hang = Column(String(200), nullable=False)
-    dia_chi = Column(String(200), nullable=False)
-    dien_thoai = Column(String(50), nullable=True)
-    email = Column(String(50), nullable=True)
+    dien_thoai = Column(String(20), nullable=False)
+    dia_chi = Column(String(200), nullable=True)
+    ngay_sinh = Column(String(200), nullable=True)
+    doanh_thu = Column(Integer(), nullable=True)
+    don_hang = relationship('DonHang', back_populates='khach_hang_dh')
 
     def __str__(self):
-        return self.ho_ten
+        return f"Khách hàng {self.ten_khach_hang} - Mã khách hàng: {self.ma_khach_hang}"
+
+
+class NhanVien(Base):
+    __tablename__ = 'NhanVien'
+    ma_nhan_vien = Column(String(50), nullable=False, primary_key=True)
+    ho_ten = Column(String(200), nullable=False)
+    dien_thoai = Column(String(50), nullable=False)
+    dia_chi = Column(String(200), nullable=True)
+    ngay_vao_lam = Column(String(200), nullable=False)
+    don_hang = relationship('DonHang', back_populates='nhan_vien_dh')
+
+    def __str__(self):
+        return (f"Nhân viên: {self.ho_ten}\n"
+                f"Số điẹn thoại: {self.dien_thoai}\n"
+                f"Địa chỉ: {self.dia_chi}\n"
+                f"Ngày vào làm: {self.ngay_vao_lam}")
 
 
 class DonHang(Base):
     __tablename__ = 'DonHang'
     ma_don_hang = Column(String(200), nullable=False, primary_key=True)
-    so_luong = Column(Integer(), nullable=False)
-    tong_tien = Column(DECIMAL(), nullable=False)
-    ngay_dat_hang = Column(DateTime(), nullable=False)
-    maKhachHang = Column(String(200), ForeignKey('KhachHang.ma_khach_hang'), nullable=True)
-    khach_hang = relationship(KhachHang, backref='don_hang')
+    ngay_tao_dh = Column(String(200), nullable=False)
+    tong_so_luong = Column(Integer(), nullable=False)
+    tong_tien = Column(Integer(), nullable=False)
+    ma_kh = Column(String(200), ForeignKey('KhachHang.ma_khach_hang'), nullable=False)
+    ma_nv = Column(String(50), ForeignKey('NhanVien.ma_nhan_vien'), nullable=False)
+    khach_hang = relationship('KhachHang', back_populates='don_hang_kh')
+    nhan_vien = relationship('NhanVien', back_populates='don_hang_nv')
 
     def __str__(self):
-        return self.ma_don_hang, self.ngay_dat_hang
-
-
-class ChiTietDonHang(Base):
-    __tablename__ = 'ChiTietDonHang'
-    ma_thong_tin_dh = Column(String(200), primary_key=True)
-    ma_dh = Column(String(200), ForeignKey('DonHang.ma_don_hang'), nullable=True)
-    ma_sp = Column(String(200), ForeignKey('SanPham.ma_san_pham'), nullable=True)
-    don_vi = Column(Integer(), nullable=False)
-    gia = Column(DECIMAL(), nullable=False)
-    product = relationship('SanPham', backref='chi_tiet_don_hang')
-    order = relationship('DonHang', backref='don_hang')
+        return f"Đơn hàng {self.ma_don_hang} - Ngày tạo: {self.ngay_tao_dh}"
 
 
 class SanPham(Base):
     __tablename__ = 'SanPham'
     ma_san_pham = Column(String(200), nullable=False, primary_key=True)
     ten_san_pham = Column(String(200), nullable=False)
-    phan_loai = Column(String(200), nullable=False)
-    gia = Column(DECIMAL(), nullable=False)
-    mo_ta = Column(String(300), nullable=True)
-    ngay_nhap_hang = Column(DateTime(), nullable=False)
-    ngay_xuat_hang = Column(DateTime(), nullable=False)
+    don_vi_tinh = Column(String(50), nullable=True)
+    nuoc_san_xuat = Column(String(50), nullable=True)
+    han_su_dung = Column(Integer(), nullable=True)
+    gia = Column(Integer(), nullable=False)
+    ctdh = relationship('ChiTietDonHang', back_populates='san_pham_ctdh')
+
+
+class ChiTietDonHang(Base):
+    __tablename__ = 'ChiTietDonHang'
+    ma_chi_tiet_dh = Column(GUID(), primary_key=True, default=uuid.uuid4)
+    so_luong_sp = Column(Integer(), nullable=False)
+    ma_dh = Column(String(200), ForeignKey('DonHang.ma_don_hang'), nullable=False)
+    ma_sp = Column(String(200), ForeignKey('SanPham.ma_san_pham'), nullable=False)
+    san_pham = relationship('SanPham', back_populates='chi_tiet_don_hang_sp')
+    don_hang = relationship('DonHang', backref='chi_tiet_don_hang')
+
+
+class KhoHang(Base):
+    __tablename__ = 'KhoHang'
+    stt = Column(Integer(), primary_key=True)   # default auto increment
+    ten_san_pham = Column(String(200), nullable=False)
+    don_vi_tinh = Column(String(50), nullable=True)
+    nuoc_san_xuat = Column(String(50), nullable=True)
+    han_su_dung = Column(Integer(), nullable=True)
+    gia = Column(Integer(), nullable=True)
+    so_luong = Column(Integer(), nullable=False)
+    trang_thai = Column(String(50), nullable=True)
+    ma_sp = Column(String(200), ForeignKey('SanPham.ma_san_pham'), nullable=False)
+    san_pham = relationship('SanPham', backref='kho_hang')
 
     def __str__(self):
-        return self.ma_san_pham, self.ten_san_pham
+        return self.__tablename__
 
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/quanlycuahang", echo=True)
+engine = create_engine("mysql+pymysql://root@localhost:3306/quanlykhohang", echo=True)
 Base.metadata.create_all(engine)
 print('Data engine connected')
